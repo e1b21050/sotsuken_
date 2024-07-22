50a2aee66b7793509b1eda10eb99b3a275153693
Koichi <e1b21050@oit.ac.jp>
2024-07-22 19:20:45 +0900

    デザインの仮設定

--- programs/ace.js
+++ programs/ace.js
@@ -7,7 +7,7 @@ function editorInit() {
     editor = ace.edit("editor");
     editor.setTheme("ace/theme/monokai"); // テーマを設定
     editor.session.setMode("ace/mode/python"); // 言語モードを設定
-    editor.setFontSize(14); // フォントサイズを設定
+    editor.setFontSize(18); // フォントサイズを設定
     editor.getSession().setTabSize(4); // タブサイズを設定
     editor.getSession().setUseWrapMode(true); // 折り返し表示を設定
 }
